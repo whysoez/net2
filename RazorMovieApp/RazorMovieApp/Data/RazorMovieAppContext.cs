bfbf83d378da8662d3bd548bb4935429616ce7bb
lucas.dinh <lucas.dinh@avepoint.com>
2024-02-01 14:01:18 +0700

    add razorPageMovie first

---
+++ RazorMovieApp/RazorMovieApp/Data/RazorMovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorMovieApp.Models;
+
+namespace RazorMovieApp.Data
+{
+    public class RazorMovieAppContext : DbContext
+    {
+        public RazorMovieAppContext (DbContextOptions<RazorMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorMovieApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ RazorMovieApp/RazorMovieApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorMovieApp.Data;
+using RazorMovieApp.Models;
+
+namespace RazorMovieApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorMovieApp.Data.RazorMovieAppContext _context;
+
+        public IndexModel(RazorMovieApp.Data.RazorMovieAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
